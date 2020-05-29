9525661b3d5dbe303f3acfdf7a273c3ed11d20ea
Arina <arina.kt@yandex.ru>
2020-05-29 17:18:48 +0400

    Initial Commit

---
+++ myGelato/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myGelato
+//
+//  Created by Арина on 29.05.2020.
+//  Copyright © 2020 Арина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
